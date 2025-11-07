5cf3d2c5795976337edc0793e3536c24d7896870
Anantharsha Dinojan <56013003+Dinojan@users.noreply.github.com>
2025-11-07 11:55:22 +0530

    remove old file

--- exit/app/Middleware/AuthMiddleware.php
+++
@@ -1,9 +0,0 @@
-<?php
-// app/Middleware/AuthMiddleware.php
-class AuthMiddleware {
-    public function handle($router) {
-        if (!isset($_SESSION['user'])) {
-            $router->redirect('/login');
-        }
-    }
-}
--- exit/app/Middleware/GuestMiddleware.php
+++
@@ -1,9 +0,0 @@
-<?php
-// app/Middleware/GuestMiddleware.php
-class GuestMiddleware {
-    public function handle($router) {
-        if (isset($_SESSION['user'])) {
-            $router->redirect('/dashboard');
-        }
-    }
-}
